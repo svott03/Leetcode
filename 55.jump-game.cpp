416a052a35057ed6585a33dbbcc0c25456ea1b3b
Steve Vott <svott@usc.edu>
2022-03-14 19:05:20 -0700

    First Problem

---
+++ 55.jump-game.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int currVal = nums.size() - 1;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (nums[i] + i  >= currVal) {
+                currVal = i;
+            }
+        }
+        return currVal == 0;
+    }
+};
+
+// @lc code=end
