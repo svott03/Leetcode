c389ab398cb5096960bd308b495f5e816bfd1f27
Steve Vott <svott@usc.edu>
2022-03-22 13:24:26 -0700

    96 unique bst

---
+++ 96.unique-binary-search-trees.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=96 lang=cpp
+ *
+ * [96] Unique Binary Search Trees
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int numTrees(int n)
+    {
+        int dp[n + 2];
+        for (int i = 0; i < n + 2; ++i)
+        {
+            dp[i] = 0;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
